a07da340dcbcc4f61a2107c2d0a69b42b40a654d
John Matthew <john@compunique.com>
2022-04-15 10:40:59 -0700

    dooh, fixed

--- trades/settings.py
+++ trades/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import dj_database_url
+import os
 
 from django.core.management.utils import get_random_secret_key
 
